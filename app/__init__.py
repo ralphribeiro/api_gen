d249e8968a6f5729793cde22f5ddbebd9b8a8c92
ralphribeiro <ralph_ribeiro@msn.com>
2020-08-27 19:00:45 -0300

    comentado

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-from flask_restplus import Api
-from flask import Blueprint
+# from flask_restplus import Api
+# from flask import Blueprint
 
-from .api.controller.usuario_controller import api as usuario_ns
+# from .api.controller.usuario_controller import api as usuario_ns
