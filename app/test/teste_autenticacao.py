a054f8c793bb097d5e5a80b349175d060701e7cd
ralphribeiro <ralph_ribeiro@msn.com>
2020-08-30 16:56:27 -0300

    mais testes

--- app/test/teste_autenticacao.py
+++ app/test/teste_autenticacao.py
@@ -2,37 +2,50 @@ import unittest
 import json
 from app.test.base import CasoDeTesteBase
 from faker import Faker
+import random
 
+nome = ''
+email = ''
+chave = ''
 
-def cria_usuario_teste() -> dict:
-    faker = Faker('pt_PT')
-    Faker.seed(1000)
-    return dict(
-        nome=faker.name(),
-        email=faker.email(),
-        chave=faker.swift()
-    )
+def cria_usuario_teste():
+    faker = Faker(['pt_PT', 'pt_BR', 'en_US'])
+    Faker.seed(0)
+
+    emails = []
+    for i in range(10):
+        emails.append(faker.email())
+
+    return faker.name(), random.choice(emails), faker.swift()
+
+def atualiza_globais():
+    global nome
+    global email
+    global chave
+    nome, email, chave = cria_usuario_teste()
+
 
 def registra_usuario(obj):
-    usu = cria_usuario_teste()
+    global nome
+    global email
+    global chave
     return obj.client.post(
         '/usuario/',
         data=json.dumps(dict(
-            nome=usu['nome'],
-            email=usu['email'],
-            chave=usu['chave']
+            nome=nome,
+            email=email,
+            chave=chave
         )),
         content_type='application/json'
     )
 
 
 def login_usuario(obj):
-    import pdb; pdb.set_trace()
     return obj.client.post(
         '/autenticacao/login',
         data=json.dumps(dict(
-            email=obj['email'],
-            chave=obj['chave']
+            email=email,
+            chave=chave
         )),
         content_type='application/json'
     )
@@ -41,6 +54,7 @@ def login_usuario(obj):
 class TesteAutenticacao(CasoDeTesteBase):
     def teste_1_usuario_registrado(self):
         with self.client:
+            atualiza_globais()
             response = registra_usuario(self)
             dado = json.loads(response.data.decode())
             self.assertTrue(dado['status'] == 'sucesso')
@@ -61,6 +75,7 @@ class TesteAutenticacao(CasoDeTesteBase):
 
     def teste_3_login_usuario_registrado(self):
         with self.client:
+            atualiza_globais()
             response = registra_usuario(self)
             dado_registro = json.loads(response.data.decode())
             self.assertTrue(dado_registro['status'] == 'sucesso')
